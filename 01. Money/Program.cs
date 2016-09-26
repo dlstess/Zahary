93a3bfbad94710211138f109dea13af8b383cea3
dlstess <zahary1981@gmail.com>
2016-09-26 05:25:49 +0300

    Mony

---
+++ 01. Money/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitcoin = int.Parse(Console.ReadLine());
+            var chines = double.Parse(Console.ReadLine());
+            var comissions = double.Parse(Console.ReadLine());
+            var sum = 0.0;
+            var dolars = 0.15 * chines;
+            
+            
+            bitcoin = bitcoin * 1168;
+            chines = dolars * 1.76;
+            comissions = (bitcoin + chines) * comissions/100;
+            sum = bitcoin + chines - comissions;
+            Console.WriteLine("Сумата на Иванчо е: {0}",sum/1.95);
+
+        }
+    }
+}
